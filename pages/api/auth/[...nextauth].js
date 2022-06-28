ad708f73ff037490c7a311338ded1f98db6d7fee
rachelnguyen <rachelnguyn@gmail.com>
2022-06-28 16:29:09 -0700

    cvhccv

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,22 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
+      authorization:
+        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET
     })
-    // ...add more providers here
   ],
+  callbacks: {
+    async jwt({token, account}) {
+      if (account) {
+        token.accessToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session(session, user) {
+      session.user = user;
+      return session;
+    },
+  },
 })
